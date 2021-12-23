63916c702356ddec4591bc00364e19c89629293d
Martin Hetschel <martin.hetschel@mach1kart.de>
2021-12-23 15:39:09 +0100

    refactor Plugin.h

--- src/plugin/AM2H_Plugin.h
+++ src/plugin/AM2H_Plugin.h
@@ -20,40 +20,29 @@ class AM2H_Plugin{
     virtual void timerPublish(AM2H_Datastore& d, PubSubClient& mqttClient, const String topic){};
     virtual void interruptPublish(AM2H_Datastore& d, PubSubClient& mqttClient, const String topic){};
 
-    String getSrv(){
-      return srv_;
-    }
-
-    String getPlugin(){
-      return plugin_;
-    }
+    const String getSrv() const { return srv_; }
+    const String getPlugin() const { return plugin_; }
 
   protected:
     String plugin_{""};
     String srv_ {"none"};
-
-    // bool isActive() { return active_;}
-    // bool active_ = false;
-    // bool interrupt_ = false;
-    // bool setupDone_ = false;
 };
 
 class AM2H_Helper {
   public:
     template <class T> const static T parse_hex(const String& string){
-        T number{0};
-        uint32_t shift{0};
-
-        int len = string.length();
-        for (int pos=0;  pos<len; ++pos){
-            char c = string.charAt(len-pos-1);
-            if ( c >= '0' && c <='9' ){ number += (c-'0') << shift; }
-            if ( c >= 'A' && c <= 'F' ){ number+= (c-'A'+10) << shift;}
-            if ( c >= 'a' && c <= 'f' ){ number+= (c-'a'+10) << shift;}
-            if ( c=='x' ) {break;}
-            shift+=4;
-        }
-        return number;
+      T number{0};
+      uint32_t shift{0};
+      int len = string.length();
+      for (int pos=0;  pos<len; ++pos){
+          char c = string.charAt(len-pos-1);
+          if ( c >= '0' && c <='9' ){ number += (c-'0') << shift; }
+          if ( c >= 'A' && c <= 'F' ){ number+= (c-'A'+10) << shift;}
+          if ( c >= 'a' && c <= 'f' ){ number+= (c-'a'+10) << shift;}
+          if ( c=='x' ) {break;}
+          shift+=4;
+      }
+      return number;
     }
 };
 
