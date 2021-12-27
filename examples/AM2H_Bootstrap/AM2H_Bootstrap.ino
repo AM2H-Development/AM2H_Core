a81f2ef788bde2e15f4b5a506b9b52ac7b8cb964
Martin Hetschel <martin.hetschel@mach1kart.de>
2021-12-27 15:29:05 +0100

    nicer...

--- examples/AM2H_Bootstrap/AM2H_Bootstrap.ino
+++ examples/AM2H_Bootstrap/AM2H_Bootstrap.ino
@@ -3,48 +3,46 @@
 //
 
 /* API-Beschreibung:
-    GET:  api/v1/get        -> {
-                                "deviceId":"myDevice9.123456789.123456789.12",
-                                "ssid":"ssid56789.123456789.123456789.12", "pw":"...",
-                                "mqttServer":"server-mh.fritz.box.123456789.123456789.123456789.",
-                                "mqttPort":1883,
-                                "ns":"ns345678"
-                             }
+  Sample requests (default IP for AP-mode:192.168.4.1)
 
-192.168.4.1
-    GET: api/v1/set?deviceId=myDevice9.123456789.123456789.12&ssid=ssid56789.123456789.123456789.12&pw=pw3456789.123456789.123456789.12&mqttServer=server-mh.fritz.box.123456789.123456789.123456789.&mqttPort=1883&ns=ns345678
+  GET:  api/v1/get        -> {
+                              "deviceId":"myDevice9.123456789.123456789.12",
+                              "ssid":"ssid56789.123456789.123456789.12", "pw":"...",
+                              "mqttServer":"server-mh.fritz.box.123456789.123456789.123456789.",
+                              "mqttPort":1883,
+                              "ns":"ns345678"
+                            }
+  GET: api/v1/set?deviceId=myDevice9.123456789.123456789.12&ssid=ssid56789.123456789.123456789.12&pw=pw3456789.123456789.123456789.12&mqttServer=server-mh.fritz.box.123456789.123456789.123456789.&mqttPort=1883&ns=ns345678
+  GET:  api/v1/status   -> {"statuslog":"[...logger content...]"}
+  GET:  api/v1/restart  -> rebooting device
 
-    GET:  api/v1/status   -> {"statuslog":"[...logger content...]"}
+  MQTT Settings:
+  home/dev/##esp01##/deviceCfg/-/sampleRate -> 15   (set sample rate to 15 s, if sampleRate==0 no TimerPublish is executed)
 
-    GET:  api/v1/restart  -> rebooting device
+  For 18B20:
+  home/dev/##esp01##/Ds18b20/##00..19##/addr -> e.g. 2895d54450013   (set OW-id of sensor)
+  home/dev/##esp01##/Ds18b20/##00..19##/loc  -> e.g. livingRoom      (location of the sensor for mqtt publish)
+  home/dev/##esp01##/Ds18b20/##00..19##/offsetTemp  -> e.g. -11      (offset temperature -1.1 °C)
 
-MQTT Settings:
-home/dev/##esp01##/deviceCfg/-/sampleRate -> 15   (set sample rate to 15 s, if sampleRate==0 no TimerPublish is executed)
+  For Sht21:
+  home/dev/##esp01##/Sht21/##00..19##/addr -> e.g. 1..7            (set I2C channel not yet implemented)
+  home/dev/##esp01##/Sht21/##00..19##/loc  -> e.g. livingRoom      (location of the sensor for mqtt publish)
+  home/dev/##esp01##/Sht21/##00..19##/offsetTemp  -> e.g. -11      (offset temperature -1.1 °C)
+  home/dev/##esp01##/Sht21/##00..19##/offsetHumidity -> e.g. -11   (offset temperature -1.1 °C)
 
-For 18B20:
-home/dev/##esp01##/Ds18b20/##00..19##/addr -> e.g. 2895d54450013   (set OW-id of sensor)
-home/dev/##esp01##/Ds18b20/##00..19##/loc  -> e.g. livingRoom      (location of the sensor for mqtt publish)
-home/dev/##esp01##/Ds18b20/##00..19##/offsetTemp  -> e.g. -11      (offset temperature -1.1 °C)
+  For Bh1750:
+  home/dev/##esp01##/Bh1750/##00..19##/addr -> e.g. 1..7            (set I2C channel)
+  home/dev/##esp01##/Bh1750/##00..19##/loc  -> e.g. livingRoom      (location of the sensor for mqtt publish)
+  home/dev/##esp01##/Bh1750/##00..19##/sensitivityAdjust  -> e.g. 69   (31..254)
 
-For Sht21:
-home/dev/##esp01##/Sht21/##00..19##/addr -> e.g. 1..7            (set I2C channel not yet implemented)
-home/dev/##esp01##/Sht21/##00..19##/loc  -> e.g. livingRoom      (location of the sensor for mqtt publish)
-home/dev/##esp01##/Sht21/##00..19##/offsetTemp  -> e.g. -11      (offset temperature -1.1 °C)
-home/dev/##esp01##/Sht21/##00..19##/offsetHumidity -> e.g. -11   (offset temperature -1.1 °C)
-
-For Bh1750:
-home/dev/##esp01##/Bh1750/##00..19##/addr -> e.g. 1..7            (set I2C channel)
-home/dev/##esp01##/Bh1750/##00..19##/loc  -> e.g. livingRoom      (location of the sensor for mqtt publish)
-home/dev/##esp01##/Bh1750/##00..19##/sensitivityAdjust  -> e.g. 69   (31..254)
-
-For Icounter:
-home/dev/##esp01##/Icounter/##00..19##/absCnt -> e.g. 112345        
-home/dev/##esp01##/Icounter/##00..19##/unitsPerTick -> e.g. 1       
-home/dev/##esp01##/Icounter/##00..19##/exponentPerTick  -> e.g. 0   (10^X)
-home/dev/##esp01##/Icounter/##00..19##/unitsPerMs -> e.g. 1   
-home/dev/##esp01##/Icounter/##00..19##/exponentPerMs  -> e.g. 0   (10^X)
-home/dev/##esp01##/Icounter/##00..19##/zeroLimit  -> e.g. 100   
-home/dev/##esp01##/Icounter/##00..19##/loc  -> e.g. gasMeter      (location of the sensor for mqtt publish)
+  For Icounter:
+  home/dev/##esp01##/Icounter/##00..19##/absCnt -> e.g. 112345
+  home/dev/##esp01##/Icounter/##00..19##/unitsPerTick -> e.g. 1
+  home/dev/##esp01##/Icounter/##00..19##/exponentPerTick  -> e.g. 0   (10^X)
+  home/dev/##esp01##/Icounter/##00..19##/unitsPerMs -> e.g. 1
+  home/dev/##esp01##/Icounter/##00..19##/exponentPerMs  -> e.g. 0   (10^X)
+  home/dev/##esp01##/Icounter/##00..19##/zeroLimit  -> e.g. 100
+  home/dev/##esp01##/Icounter/##00..19##/loc  -> e.g. gasMeter      (location of the sensor for mqtt publish)
 
 */
 #include <AM2H_Core.h>
@@ -61,14 +59,11 @@ AM2H_Bh1750 bh1750("Bh1750","envsense");
 AM2H_Icounter icounter("Icounter","counter");
 
 AM2H_Plugin* plugins[] {&ds18b20, &sht21, &bme680, &bh1750, &icounter, nullptr};
-
 AM2H_Core core(plugins);
 
 // ----------
 // ---------- setup() --------------------------------------------------------------------------
 // ---------------------------------------------------------------------------------------------
-
-
 void setup()
 {
   core.setupCore();
